96336fb6454c2efd12503e44e8a660a169e565fd
Michael Couch <michaelcouch@MacBook-Pro.local>
2024-03-17 20:48:21 -0700

    formatted flip camera button

--- App.js
+++ App.js
@@ -37,20 +37,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'flex-end'
   },
   camera: {
-    width: '60%',
-    height: '60%'
+    width: '100%',
+    height: '100%',
+    justifyContent: 'flex-end'
   },
   buttonContainer: {
 
   },
   button: {
-    backgroundColor: 'blue',
-    alignItems: 'center'
+    alignItems: 'center',
+    marginBottom: 50
   },
   text: {
-    color: 'white'
+    color: 'white',
+    fontWeight: 'bold'
   }
 });
