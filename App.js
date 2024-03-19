9078be133ca70063696542c9c5ab41358a7217b9
Michael Couch <michaelcouch@MacBook-Pro.local>
2024-03-18 21:17:39 -0700

    take picture now

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import { Camera, CameraType } from 'expo-camera';
 import { useState } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View, useWindowDimensions } from 'react-native';
+import { Button, StyleSheet, Text, TouchableOpacity, View, useWindowDimensions,ImageBackground, Image } from 'react-native';
 
 export default function App() {
 
   const [type, setType] = useState(CameraType.back);
   const [permission, requestPermission] = Camera.useCameraPermissions();
 
+  const [previewVisible, setPreviewVisible] = useState(false);
+
+  const [capturedImage, setCapturedImage] = useState(null);
+
   const {width} = useWindowDimensions();
   const height = Math.round((width*4)/3);
+
+  let camera: Camera;
+
+
 
   console.log('permission at startup: ' + permission);
 
@@ -23,8 +31,33 @@ export default function App() {
     setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
   }
 
+  async function takePicture() {
+    console.log('take picture');
+    const photo = await camera.takePictureAsync();
+
+    setCapturedImage(photo);
+    setPreviewVisible(true);
+
+    console.log(photo);
+  }
+
   return (
     <View style={styles.container}>
+      {previewVisible && capturedImage ? 
+      <View style={{
+          flex: 1,
+          backgroundColor: 'transparent',
+          alignItems: 'center',
+          width: '100%',
+          height: '100%'}}>
+        <ImageBackground 
+        style={{ 
+          flex:1,
+          height: height,
+          width: '100%'
+          }} source={{uri: capturedImage.uri}}/>
+      </View>
+      :
       <Camera 
         ratio="4:3"
         style={{
@@ -32,15 +65,23 @@ export default function App() {
           width: "100%",
           justifyContent: 'flex-end'
         }} 
-        type={type}>
+        type={type}
+        ref={(r)=> {
+          camera = r
+        }}
+        >
         <View style={styles.buttonContainer}>
           <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
             <Text style={styles.text}>Flip Camera</Text>
           </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={takePicture}>
+            <Text style={styles.text}>Take Picture</Text>
+          </TouchableOpacity>
         </View>
       </Camera>
+      }
     </View>
-  );
+      );
 }
 
 const styles = StyleSheet.create({
