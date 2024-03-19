fa910cb0b89ab557da6a06410df9016719a71f12
Michael Couch <michaelcouch@MacBook-Pro.local>
2024-03-18 20:30:15 -0700

    further camera viewer mods

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { Camera, CameraType } from 'expo-camera';
 import { useState } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Button, StyleSheet, Text, TouchableOpacity, View, useWindowDimensions } from 'react-native';
 
 export default function App() {
 
   const [type, setType] = useState(CameraType.back);
   const [permission, requestPermission] = Camera.useCameraPermissions();
+
+  const {width} = useWindowDimensions();
+  const height = Math.round((width*4)/3);
 
   console.log('permission at startup: ' + permission);
 
@@ -22,7 +25,14 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Camera style={styles.camera} type={type}>
+      <Camera 
+        ratio="4:3"
+        style={{
+          height: height,
+          width: "100%",
+          justifyContent: 'flex-end'
+        }} 
+        type={type}>
         <View style={styles.buttonContainer}>
           <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
             <Text style={styles.text}>Flip Camera</Text>
@@ -37,7 +47,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'flex-start'
+    justifyContent: 'flex-start',
+    paddingTop: 50
   },
   camera: {
     width: '100%',
